745e213a89def1e77076b32158b9ed229de2889c
Jenna-Zing <47491259+Jenna-Zing@users.noreply.github.com>
2025-03-10 21:43:33 -0500

    added Harold's feedback: add operationNum, and console log for 3 days additional attendance.

--- detective.js/detective.js
+++ detective.js/detective.js
@@ -1,12 +1,12 @@
-function mysteryOperation() {
-	const outcome = Math.random(); // Generates a random number between 0 and 1.
+function mysteryOperation(operationNum) {
+    const outcome = Math.random(); // Generates a random number between 0 and 1.
 
-	if (outcome < 0.5) {
-		console.log("The operation is completed successfully!");
-	}
-	else {
-		throw new Error("The operation is failed mysteriously!");
-	}
+    if (outcome < 0.5) {
+        console.log(`Operation #${operationNum} is completed successfully! 13 vacation days added.`);
+    }
+    else {
+        throw new Error(`Operation #${operationNum} failed mysteriously! 1 vacation day added.`);
+    }
 }
 
 const numberOfOperations = 20;
@@ -18,18 +18,19 @@ const daysForFailure = 1;
 const daysForAttending = 3;
 
 for (let i = 0; i < numberOfOperations; i++) {
-	try {
-		mysteryOperation();
-		// only reaches here if the mission was successful
-		numberOfVacationDaysEarned += daysForSuccess;
-	} catch (error) {
-		// only reaches here if the error was thrown for mission failure
-		console.log(error.message);
-		numberOfVacationDaysEarned += daysForFailure;
-	} finally {
-		// "regardless of the outcome", this always runs.
-		numberOfVacationDaysEarned += daysForAttending;
-	}
+    try {
+        mysteryOperation(i + 1);
+        // only reaches here if the mission was successful
+        numberOfVacationDaysEarned += daysForSuccess;
+    } catch (error) {
+        // only reaches here if the error was thrown for mission failure
+        console.log(error.message);
+        numberOfVacationDaysEarned += daysForFailure;
+    } finally {
+        // "regardless of the outcome", this always runs.
+        console.log("Three vacation days added for your attendance!");
+        numberOfVacationDaysEarned += daysForAttending;
+    }
 }
 
 console.log("You have earned ", numberOfVacationDaysEarned, " days of vacation!");
